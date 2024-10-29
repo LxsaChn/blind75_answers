e3bba071bc801d341f5c2a032c240b3526e127e4
Lisa Chen <lycheen915@gmail.com>
2024-10-28 17:24:46 -0700

    updated

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -4,9 +4,9 @@
 #         self.val = val
 #         self.next = next
 #############################
-# RECURSIVE METHOD FOR THIS
 
 class Solution(object):
+    # RECURSIVE METHOD FOR THIS
     def mergeTwoLists(self, list1, list2):
         """
         :type list1: Optional[ListNode]
@@ -26,6 +26,37 @@ class Solution(object):
             new_list.next = self.mergeTwoLists(list1.next, list2)
         
         return new_list
+    
+    # ITERATIVE APPROACH
+    def mergeTwoListsIterative(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        new_list = ListNode(0)
+        current = new_list
+
+        while list1 is not None and list2 is not None:
+            if list1 is None or list1.val > list2.val:
+                current.next = list2
+                list2 = list2.next
+            else:
+                current.next = list1
+                list1 = list1.next
+            current = current.next
+
+        if list1 is not None:
+            current.next = list1
+        if list2 is not None:
+            current.next = list2
+        
+        return new_list.next
 
 
 
